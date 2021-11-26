2be61ea297ae49a760e0a9e1b39cfca7ea393ae0
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2021-11-26 14:58:37 +0800

    Update dashboard.dart

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -4,7 +4,15 @@ import 'package:flutter/material.dart';
 
 import 'package:barangay_system_resident/profile.dart';
 
-class Dashboard extends StatelessWidget {
+bool shouldDisplay = false;
+final puposeController = TextEditingController();
+
+class Dashboard extends StatefulWidget {
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
   var currentUser = FirebaseAuth.instance.currentUser;
 
   @override
@@ -32,6 +40,7 @@ class Dashboard extends StatelessWidget {
             'birthdate': Profile.birthdate,
             'birthplace': Profile.birthplace,
             'civil_status': Profile.civil_status,
+            'purpose': puposeController.text,
           })
           .then((value) => print("Request Added"))
           .catchError((error) => print("Failed to add Request: $error"));
@@ -46,7 +55,39 @@ class Dashboard extends StatelessWidget {
                 'Select certificate you want to issue:',
                 style: TextStyle(fontSize: 18, fontFamily: 'Calibri'),
               )),
-          dropdown(),
+          Container(
+              margin: EdgeInsets.only(bottom: 5, top: 5, left: 10, right: 10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  color: Colors.white),
+              child: DropdownButtonFormField<String>(
+                value: value,
+                decoration: InputDecoration(prefixIcon: Icon(Icons.article)),
+                iconSize: 36,
+                icon: Icon(
+                  Icons.arrow_drop_down,
+                  color: Colors.black,
+                ),
+                hint: Text("Select certificate"),
+                style: TextStyle(color: Colors.black),
+                isExpanded: true,
+                items: items.map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (dropdownValue) {
+                  setState(() {
+                    shouldDisplay = !shouldDisplay;
+                    value = dropdownValue;
+                  });
+                },
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          if (value == "Indigency") conditionalTextField(),
           SizedBox(
             height: 20,
           ),
@@ -59,7 +100,7 @@ class Dashboard extends StatelessWidget {
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
             child: Text(
-              "Submit",
+              "Submit Request",
               style: TextStyle(
                 fontWeight: FontWeight.w600,
                 fontSize: 18,
@@ -71,64 +112,59 @@ class Dashboard extends StatelessWidget {
       ),
     );
   }
-}
 
-final items = [
-  "Clearance",
-  "Indegency",
-  "Building Permit",
-  "Residency",
-  "Wiring",
-  "Fencing",
-  "Water",
-  "Excavation",
-  "Blotter"
-];
-String? value;
+  final items = [
+    "Clearance",
+    "Indigency",
+    "Building Permit",
+    "Residency",
+    "Wiring",
+    "Fencing",
+    "Water",
+    "Excavation",
+    "Blotter"
+  ];
+  String? value;
 //Widget for dropdown
-Widget dropdown() {
-  return Container(
-      margin: EdgeInsets.only(bottom: 5, top: 5, left: 10, right: 10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-          color: Colors.white),
-      child: DropdownButtonFormField<String>(
-        value: value,
-        decoration: InputDecoration(prefixIcon: Icon(Icons.article)),
-        iconSize: 36,
-        icon: Icon(
-          Icons.arrow_drop_down,
-          color: Colors.black,
-        ),
-        hint: Text("Select certificate"),
-        style: TextStyle(color: Colors.black),
-        isExpanded: true,
-        items: items.map((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
-        onChanged: (s) {
-          value = s;
-        },
-      ));
-}
+  // Widget dropdown() {
+  //   return ;
+  // }
 
 //Curved container
-class HeaderCurvedContainer extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()..color = Color(0xff555555);
-    Path path = Path()
-      ..relativeLineTo(0, 150)
-      ..quadraticBezierTo(size.width / 2, 225, size.width, 150)
-      ..relativeLineTo(0, -150)
-      ..close();
+// class HeaderCurvedContainer extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     Paint paint = Paint()..color = Color(0xff555555);
+//     Path path = Path()
+//       ..relativeLineTo(0, 150)
+//       ..quadraticBezierTo(size.width / 2, 225, size.width, 150)
+//       ..relativeLineTo(0, -150)
+//       ..close();
 
-    canvas.drawPath(path, paint);
+//     canvas.drawPath(path, paint);
+//   }
+
+//   @override
+//   bool shouldRepaint(CustomPainter oldDelegate) => false;
+// }
+
+  Widget conditionalTextField() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 5, top: 5, left: 10, right: 10),
+      child: TextField(
+        controller: puposeController,
+        decoration: InputDecoration(
+            labelText: "Purpose",
+            fillColor: Colors.white,
+            filled: true,
+            hintText: "Purpose of Indigency",
+            contentPadding: EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+            ),
+            border:
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+      ),
+    );
   }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
