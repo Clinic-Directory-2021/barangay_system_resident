f197eda02e6e1d401f18066fdacad6127a6746fb
Kenneth-Galvez-BSIT3N <galvez.kenneth.p.8842@gmail.com>
2021-12-09 22:10:49 +0800

    Some changes

--- lib/signup.dart
+++ lib/signup.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
